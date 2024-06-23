ae545a4a2cb4aac577eb49b1241b8ec8f93f0ea2
Arturosq007 <126190714+Arturosq007@users.noreply.github.com>
2024-06-23 11:24:02 -0500

    Actualizando ejercicio2: Verificar que numero es menor

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -2,12 +2,18 @@
 using namespace std;
 
 int main(){
-    int n1, n2, c = 0, i = 1;
+    int n1, n2, c = 0, i = 1, menor;
 
     cout<<"Ingrese el primer numero: ";cin>>n1;
     cout<<"Ingrese el segundo numero: ";cin>>n2;
 
-    while (i <= n1 && i <= n2){
+    if (n1 < n2){
+        menor = n1;
+    }else{
+        menor = n2;
+    }
+
+    while (i <= menor){
         if (n1 % i == 0 && n2 % i == 0){
             c++;
         }
