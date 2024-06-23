eaf3096809504262ac47a2b6dcd22c2dd59d3c8a
Arturosq007 <126190714+Arturosq007@users.noreply.github.com>
2024-06-22 19:13:29 -0500

    Actualizando ejercicio1: Preguntar si se quiere realizar otra operacion

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -2,42 +2,59 @@
 using namespace std;
 
 int main(){
-    float n1,n2,r;
-    char op;
-    bool validacion = true;
+    float n1,n2,r = 0;
+    char op,otra;
+    bool validacion = true, nuevaOperacion;
 
-    cout<<"Ingrese el primer numero: ";cin>>n1;
-    cout<<"Ingrese el segundo numero: ";cin>>n2;
-    cout<<"Tienes las siguientes operaciones a elegir: ";
-    cout<<"Suma (+),    Resta(-),    Multiplicacion(*),     Division(/):  ";cin>>op;
+    do{
+        cout<<"Ingrese el primer numero: ";cin>>n1;
+        cout<<"Ingrese el segundo numero: ";cin>>n2;
+        cout<<"Tienes las siguientes operaciones a elegir: ";
+        cout<<"Suma (+),    Resta(-),    Multiplicacion(*),     Division(/):  ";cin>>op;
 
-    switch (op){
-    case '+':
-        r = n1 + n2;
-        break;
-    case '-':
-        r = n1 - n2;
-        break;
-    case '*':
-        r = n1 * n2;
-        break;
-    case '/':
-        if (n2 == 0){
-            cout<<"El numero no puede ser dividido entre 0";
+        switch (op){
+        case '+':
+            r = n1 + n2;
+            break;
+        case '-':
+            r = n1 - n2;
+            break;
+        case '*':
+            r = n1 * n2;
+            break;
+        case '/':
+            if (n2 == 0){
+                cout<<"El numero no puede ser dividido entre 0";
+                validacion = false;
+            }else{
+                r = n1 / n2;
+            }
+            break;        
+        default:
+            cout<<"Ingrese una operacion valida";
             validacion = false;
-        }else{
-            r = n1 / n2;
+            break;
         }
-        break;        
-    default:
-        cout<<"Ingrese una operacion valida";
-        validacion = false;
-        break;
-    }
 
-    if (validacion){
-        cout<<"El resultado es: "<< r <<endl;    
-    }
+        if (validacion){
+            cout<<"El resultado es: "<< r <<endl;
+
+            cout<<"¿Desea realizar otra operacion?: "<<endl;
+            cout<<"si (s),    no(n): ";cin>>otra;
+            if (otra == 's'){
+                nuevaOperacion = true;
+            }else if (otra == 'n'){
+                nuevaOperacion = false;
+            }else{
+                cout<<"Ingrese una opcion vallida";
+                nuevaOperacion = false;
+            }
+            
+                
+        }
+    } while (nuevaOperacion == true);
+    
+    
     
     return 0;
 }
