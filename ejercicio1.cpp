6bf550456e2d0724d0cabd0451058a3b4d5ca845
Arturosq007 <126190714+Arturosq007@users.noreply.github.com>
2024-06-22 19:33:55 -0500

    Actualizando ejercicio1: Agregar do-while en la ultima validacion

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -41,22 +41,25 @@ int main(){
         if (validacion){
             cout<<"El resultado es: "<< r <<endl;
 
-            cout<<"¿Desea realizar otra operacion?:   si (s)  no (n): ";cin>>otra;
-            if (otra == 's'){
-                nuevaOperacion = true;
-            }else if (otra == 'n'){
-                nuevaOperacion = false;
-            }else{
-                cout<<"Ingrese una opcion valida"<<endl;
-                nuevaOperacion = false;
-            }        
+            do{
+                cout<<"¿Desea realizar otra operacion?:   si (s)  no (n): ";cin>>otra;
+
+                if (otra == 's'){
+                    nuevaOperacion = true;
+                }else if (otra == 'n'){
+                    cout<<"Cerrando el programa"<<endl;
+                    nuevaOperacion = false;
+                }else{
+                    cout<<"Ingrese una opcion valida"<<endl;
+                }        
+           
+            } while (otra != 's' && otra != 'n');
+            
         }else{
             nuevaOperacion = true; 
         }
 
     } while (nuevaOperacion == true);
-    
-    
     
     return 0;
 }
